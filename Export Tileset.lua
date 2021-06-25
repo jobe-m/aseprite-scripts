a6a4030aab7a0c530d0a6848950acce3c7c3e196
Marko Koschak <marko.koschak@tisno.de>
2021-06-25 14:48:21 +0200

    Add Export Tileset script for aseprite v1.3
    This script is a modification of the Save As Extruded Sprite Sheet
    script. It will export the tileset of a tilemap layer into a png file.
    It is possible to switch extrusion of tiles in the final exported
    tilemap on and off.

---
+++ Export Tileset.lua
@@ -0,0 +1,66 @@
+if TilesetMode == nil then return app.alert "Use Aseprite v1.3"  end
+
+local lay = app.activeLayer
+if not lay.isTilemap then return app.alert "No active tilemap layer" end
+
+local tileset = lay.tileset
+
+local dlg = Dialog("Export Tileset")
+dlg:file{ id="filename", label="Export to Tilemap file:", save=true, focus=true,
+          filename=app.fs.joinPath(app.fs.filePath(lay.sprite.filename), lay.name .. ".png") }
+ :label{ label="Number of Tiles to be exported", text=tostring(#tileset) }
+
+ :separator()
+ :number { id="tilemap_cols", label="Number of columns in Tilemap:", text="14" }
+ :check { id="ask_overwrite", label="Ask before overwrite existing Tilemap file", selected=true }
+ :check { id="tiles_extruded", label="Extrude Tiles in Tilemap", selected=true }
+ :check { id="keep_sprite_frames", label="Keep generated sprite frames open", selected=false }
+ :check { id="open_generated", label="Open generated Tilemap", selected=false }
+
+ :separator()
+ :button{ text="&Export", focus=true, id="ok" }
+ :button{ text="&Cancel" }
+ :show()
+
+-- Data validation
+local data = dlg.data
+if not data.ok then return end
+
+local spec = lay.sprite.spec
+local grid = tileset.grid
+local size = grid.tileSize
+
+-- Create a new sprite with the dimension of one single tile
+local newSpr = Sprite(size.width, size.height, lay.sprite.colorMode)
+
+-- give the new sprite the same palette as the source sprite
+newSpr:setPalette(lay.sprite.palettes[1])
+
+-- Copy each tile into a new frame of sprite
+for i = 0, #tileset - 1 do
+  local tile = tileset:getTile(i)
+  newSpr.cels[i + 1].image = tile
+  -- Add a new frame
+  app.command.NewFrame { ["content"]="current" }
+end
+
+if app.apiVersion >= 3 then
+  newSpr.filename = "sprite frames"
+end
+
+-- Export sprite frames as an extruded sprite sheet
+app.command.ExportSpriteSheet{
+  ui=false,
+  askOverwrite=data.ask_overwrite,
+  type=SpriteSheetType.ROWS,
+  columns=data.tilemap_cols,
+  textureFilename=data.filename,
+  extrude=data.tiles_extruded,
+  openGenerated=data.open_generated
+}
+
+-- Close sprite frames again
+if data.keep_sprite_frames == false then
+  newSpr:close()
+end
+
